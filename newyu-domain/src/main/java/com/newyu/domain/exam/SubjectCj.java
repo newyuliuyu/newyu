332cec95fdba14c564c0988b896aa76782000a9d
liuyu <306487103@qq.com>
2019-04-12 16:19:07 +0800

    添加考试对象结构

---
+++ newyu-domain/src/main/java/com/newyu/domain/exam/SubjectCj.java
@@ -0,0 +1,14 @@
+package com.newyu.domain.exam;
+
+/**
+ * ClassName: SubjectCj <br/>
+ * Function:  ADD FUNCTION. <br/>
+ * Reason:  ADD REASON(可选). <br/>
+ * date: 19-4-12 下午4:15 <br/>
+ *
+ * @author liuyu
+ * @version v1.0
+ * @since JDK 1.7+
+ */
+public class SubjectCj {
+}
