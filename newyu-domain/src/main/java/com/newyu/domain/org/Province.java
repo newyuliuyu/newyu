13105f482c9291dce39254d2be8d7cae3342ff70
liuyu <306487103@qq.com>
2019-04-12 16:18:39 +0800

    添加组织结构对象

---
+++ newyu-domain/src/main/java/com/newyu/domain/org/Province.java
@@ -0,0 +1,14 @@
+package com.newyu.domain.org;
+
+/**
+ * ClassName: Province <br/>
+ * Function:  ADD FUNCTION. <br/>
+ * Reason:  ADD REASON(可选). <br/>
+ * date: 19-4-12 下午4:16 <br/>
+ *
+ * @author liuyu
+ * @version v1.0
+ * @since JDK 1.7+
+ */
+public class Province extends Org {
+}
