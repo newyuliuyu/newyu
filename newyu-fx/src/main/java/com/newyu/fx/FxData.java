80b77e2fd4864d662ef24c88d5a2b03fde1da2d4
liuyu <306487103@qq.com>
2019-04-15 17:44:37 +0800

    增加结果分析数据

---
+++ newyu-fx/src/main/java/com/newyu/fx/FxData.java
@@ -0,0 +1,23 @@
+package com.newyu.fx;
+
+import lombok.*;
+
+/**
+ * ClassName: FxData <br/>
+ * Function:  ADD FUNCTION. <br/>
+ * Reason:  ADD REASON(可选). <br/>
+ * date: 19-4-15 下午5:43 <br/>
+ *
+ * @author liuyu
+ * @version v1.0
+ * @since JDK 1.7+
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FxData {
+    private String name;
+    private Object value;
+}
