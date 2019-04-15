72290f3f8066351a3d5595538fc3cdffc68c6b5a
liuyu <306487103@qq.com>
2019-04-15 17:51:32 +0800

    构建分析结果数据

--- newyu-fx/src/main/java/com/newyu/fx/FxResult.java
+++ newyu-fx/src/main/java/com/newyu/fx/FxResult.java
@@ -1,5 +1,13 @@
 package com.newyu.fx;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * ClassName: FxResult <br/>
  * Function:  ADD FUNCTION. <br/>
@@ -10,5 +18,19 @@ package com.newyu.fx;
  * @version v1.0
  * @since JDK 1.7+
  */
+@Getter
+@Setter
 public class FxResult {
+
+    private GroupValue groupValue;
+    private Map<String, FxData> fxDataMap = Maps.newHashMap();
+
+    public FxResult add(FxData fxData) {
+        fxDataMap.put(fxData.getName(), fxData);
+        return this;
+    }
+
+    public List<FxData> toList() {
+        return Lists.newArrayList(fxDataMap.values());
+    }
 }
