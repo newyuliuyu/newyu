370b3efbda57efe581ae44b39c4767e13ba28861
liuyu <306487103@qq.com>
2019-04-19 11:02:09 +0800

    创建考试服务类

--- newyu-service-api/src/main/java/com/newyu/service/ExamService.java
+++ newyu-service-api/src/main/java/com/newyu/service/ExamService.java
@@ -1,5 +1,10 @@
 package com.newyu.service;
 
+import com.newyu.domain.exam.Exam;
+import com.newyu.domain.exam.ExamState;
+
+import java.util.List;
+
 /**
  * ClassName: ExamService <br/>
  * Function:  ADD FUNCTION. <br/>
@@ -11,4 +16,46 @@ package com.newyu.service;
  * @since JDK 1.7+
  */
 public interface ExamService {
+
+    /**
+     * 创建一次考试
+     *
+     * @param exam
+     * @return
+     */
+    boolean create(Exam exam);
+
+    /**
+     * 修改考试状态
+     *
+     * @param examId
+     * @param examState
+     * @return
+     */
+    int update(long examId, ExamState examState);
+
+    /**
+     * 删除一次考试
+     *
+     * @param exam
+     * @return
+     */
+    int delete(Exam exam);
+
+    /**
+     * 获取一次考试信息
+     *
+     * @param examId
+     * @return
+     */
+    Exam get(long examId);
+
+    /**
+     * 获取当前考试的历次考试
+     * @param exam
+     * @return
+     */
+    List<Exam> queryHistoryExam(long exam);
+
+
 }
