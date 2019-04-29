50e43ecc6358e59cf38a8f129f0c1139fde29f98
liuyu <306487103@qq.com>
2019-04-29 17:35:28 +0800

    增加resulfulAPI工程

---
+++ newyu-resulful-api/src/main/java/com/newyu/controller/NewyuResulfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.newyu.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyuResulfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewyuResulfulApiApplication.class, args);
+    }
+
+}
---
+++ newyu-resulful-api/src/test/java/com/newyu/controller/NewyuResulfulApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newyu.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewyuResulfulApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
