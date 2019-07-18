a067a2afad440dd4000505afac873e1e1eb07338
liuyu <306487103@qq.com>
2019-07-18 10:47:55 +0800

    只有总分成绩的分析

---
+++ newyu-es/src/main/java/com/newyu/es/NewyuEsApplication.java
@@ -0,0 +1,13 @@
+package com.newyu.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyuEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewyuEsApplication.class, args);
+    }
+
+}
---
+++ newyu-es/src/test/java/com/newyu/es/NewyuEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newyu.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewyuEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
