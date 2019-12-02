fa9f88365ce90c986ebac59007d04422ca371c79
liuyu <306487103@qq.com>
2019-12-02 16:12:40 +0800

    监控系统配额在

---
+++ newyu-webflux/src/main/java/com/newyu/newyuwebflux/NewyuWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.newyu.newyuwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyuWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewyuWebfluxApplication.class, args);
+    }
+
+}
