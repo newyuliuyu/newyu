b4ca264a424814c106447f4ddca788ee8ee04de1
liuyu <306487103@qq.com>
2019-12-02 16:12:56 +0800

    监控系统配额在

---
+++ newyu-webflux/src/test/java/com/newyu/newyuwebflux/NewyuWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newyu.newyuwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewyuWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
